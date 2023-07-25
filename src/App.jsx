d6e3d4ae9032eeb8d77a24eeda8980bc7af570bd
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-07-25 11:51:11 +0100

    add main parts of the project and start work

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      welcom back!!!?''
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      HOme
+    </div>
+  )
+}
+
+export default Home
