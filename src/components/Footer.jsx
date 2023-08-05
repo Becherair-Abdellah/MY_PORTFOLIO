80e0fef660107fbaf81c8569b37624c6278668c3
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-08-05 14:00:06 +0100

    its okey

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import SocialMedia from './home/SocialMedia'
-
 function Footer({year,name}) {
 
   return (
@@ -10,5 +8,4 @@ function Footer({year,name}) {
     </div>
   )
 }
-
 export default Footer
