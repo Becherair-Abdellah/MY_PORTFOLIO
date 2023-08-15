3224388f1c3762aa116db8c552048dcadb13d292
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-08-15 11:29:19 +0100

    finish of product

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import {FiDownload} from 'react-icons/fi'
+import srcCv from '../assets/files/cv.pdf'
 function Button() {
         const buttonVariants = {
           rest: {
@@ -11,12 +12,14 @@ function Button() {
           },
         };
   return (
+    <a href={srcCv} download>
     <motion.div  variants={buttonVariants}
     whileHover="hover"
     whileTap="tap"
      className='flex items-center gap-3 text-white font-bold text-xl bg-redColor w-fit rounded-full  px-4 py-2 cursor-pointer'>
       Download CV <FiDownload size={25}/>
     </motion.div>
+    </a>
   )
 }
 
--- src/components/home/SocialMedia.jsx
+++ src/components/home/SocialMedia.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { BsLinkedin, BsGithub,BsTriangle,BsStar } from 'react-icons/bs'
+import { BsLinkedin, BsGithub, BsTriangle, BsStar } from 'react-icons/bs'
 import { MdAlternateEmail } from 'react-icons/md'
 import { FaTwitterSquare } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 function SocialMedia() {
   return (
-<div className='flex mt-8 w-fit items-center gap-3' >
-          <BsLinkedin size={25} className='text-TextColor cursor-pointer' />
-          <MdAlternateEmail size={25} className='text-TextColor cursor-pointer' />
-          <BsGithub size={25} className='text-TextColor cursor-pointer' />
-          <FaTwitterSquare size={25} className='text-TextColor cursor-pointer' />
-        </div>
+    <div className='flex mt-8 w-fit items-center gap-3' >
+      <Link to="https://www.linkedin.com/in/abdellah-becherair-198825263/">
+        <BsLinkedin size={25} className='text-TextColor cursor-pointer' />
+      </Link>
+      <Link to="mailto:abdellah.becherair04@gmail.com">
+        <MdAlternateEmail size={25} className='text-TextColor cursor-pointer' />
+      </Link>
+
+      <Link to="https://github.com/Becherair-Abdellah" >
+        <BsGithub size={25} className='text-TextColor cursor-pointer' />
+      </Link>
+
+      <Link to="https://twitter.com/AbdellahBecher">
+        <FaTwitterSquare size={25} className='text-TextColor cursor-pointer' />
+      </Link>
+    </div>
   )
 }
 
