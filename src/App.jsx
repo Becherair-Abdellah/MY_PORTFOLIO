6e4c97363b924985883a88982fe32e93f89725b5
abdellah <abdeallahdz2004@gmail.com>
2024-03-03 11:29:45 +0100

    editing a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import store from './redux/store'
 import Dashboard from './dashboard/Dashboard'
 import Portfolio from './components/frontend/Portfolio'
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 // import Login from './dashboard/Login'
 // import ResetPassword from './dashboard/ResetPassword' 
 function App() {
@@ -14,7 +15,8 @@ function App() {
  <div className='w-full p-3 text-md bg-gradient-to-r from-blue-700 text-white to-red-600 text-center'>
         <p>I write more articles <span className='hidden md:inline'>About Frontend exactly react js and next</span> on my <a href="https://www.linkedin.com/in/abdellahbecherair/" className='font-bold underline'>LinkedIn</a></p>
     </div>
- <div className='md:w-[85%] mx-auto mt-2 md:mt-6 p-3'>
+ <div className='md:w-[85%] mx-auto md:mt-6 p-3'>
+  <Navbar/>
  <Outlet/>
  </div>
  <div className='w-full bg-[#262626]  '>
