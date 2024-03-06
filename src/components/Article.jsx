846e5f004145118a80f2e888dc5bec79830c902a
abdellah <abdeallahdz2004@gmail.com>
2024-03-06 16:11:10 +0100

    add new component of article pages

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,46 +3,16 @@ function Article() {
   return (
     <>
       <div
-        class="top-container border-b-2 mb-10 md:mb-14"
+        class="top-container border-b-2 mb-10 md:mb-14 py-5 md:py-10"
         data-astro-cid-wu5dj4rx=""
       >
-        <main
-          class="flex px-0 flex-col-reverse md:flex-row md:-my-10 md:my-0 md:flex-row"
-          data-astro-cid-wu5dj4rx=""
-        >
-          {/* <div
-            id="page-header-content"
-            class="md:w-9/12 pr-0 md:pr-5"
-            data-astro-cid-wu5dj4rx=""
-          >
-            <h1
-              class="text-4xl md:text-5xl leading-snug md:leading-snug mt-10 md:mt-5 font-bold text-center md:text-left"
-              data-astro-cid-bvzihdzo=""
-            >
-              How to Optimize the Web for the Worst User Conditions?
-            </h1>
-            <h2
-              class="post-date text-lg my-7 leading-10 flex flex-col-reverse md:flex-row items-center md:items-start md:gap-3"
-              data-astro-cid-bvzihdzo=""
-            >
-              <a
-                href="/blog/performance"
-                class="capsule leading-9"
-                data-astro-cid-53hm25xo=""
-              >
-                <div class=" gap-2 inline-flex" data-astro-cid-53hm25xo="">
-                  <div class="label" data-astro-cid-53hm25xo="">
-                    Performance
-                  </div>
-                </div>
-              </a>
-              <div class="flex gap-3" data-astro-cid-bvzihdzo="">
-                <time datetime="2024-02-21T00:00:00.000Z">21 Feb 2024</time> •{" "}
-                <span data-astro-cid-bvzihdzo="">10 min read</span>
-              </div>
-            </h2>
-          </div> */}
-          <h1 className="text-white text-2xl text-center w-full py-10">No Articles Here</h1>
+        <main>
+          <h1 className="text-white md:text-5xl text-4xl font-bold leading-[3.5rem] md:text-start md:leading-[4rem] text-center w-full py-4">What is the difference between CSR and SSR?</h1>
+          <div className="flex items-center gap-5 justify-center md:justify-start">
+          <a href="" className="bg-[#454648] text-white px-4 py-1 rounded-xl hover:text-red-600">Life</a>
+          <h2 className="text-white text-xl"><span className="mr-10">21 Feb 2024</span><span>10 min read</span></h2>
+          </div>
+          
         </main>
       </div>
     </>
